314383f0e26154ee7af6b4e554e2d68a8965c80b
KrishnaVamsiReddi <reddikrishnavamsi@gmail.com>
2025-04-11 14:23:08 +0530

    cart functionality

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -9,16 +9,34 @@ const ShopContextProvider = ({ children }) => {
   const addToCart = (itemId)=>{
     setCart((prev)=> ({...prev, [itemId]: prev[itemId]+1}))
 
-    // console.log(cart)
+    console.log(cart)
   }
 
   const removeFromCart = (itemId)=>{
     setCart((prev)=> ({...prev, [itemId]: prev[itemId]-1}))
   }
 
+  const getTotalCartAmount = ()=>{
+    let totalAmount = 0;
+    for(const item in cart){
+      if(cart[item]>0){
+        let itemInfo = all_product.find((p)=>p.id === Number(item))
+        totalAmount += itemInfo.new_price * cart[item];
+      }
+    }
+
+    return totalAmount;
+  }
+
+  const getTotalCartItems = ()=>{
+    let totalItems = 0;
+    for(let item in cart) totalItems += cart[item]
+    return totalItems
+  }
+
 
   return (
-    <ShopContext.Provider value={{ all_product, cart, addToCart, removeFromCart }}>
+    <ShopContext.Provider value={{ all_product, cart, addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems}}>
       {children}
     </ShopContext.Provider>
   );
